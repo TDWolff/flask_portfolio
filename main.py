f15524cab4f09bfa33f8d2c3775dff4b89676c4d
TDWolff <torin.dean.wolff@gmail.com>
2024-08-25 17:05:42 -0700

    Removing redundant scripts and simplifying it to a basic website

--- main.py
+++ main.py
@@ -8,36 +8,21 @@ import os
 
 
 # import "packages" from "this" project
-from __init__ import app, db, cors  # Definitions initialization
+from __init__ import app, cors  # Definitions initialization
 
+# from api.user import user_api # Blueprint import api definition
 
-# setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
-from api.player import player_api
-# database migrations
-from model.users import initUsers
-from model.players import initPlayers
+# # setup App pages
+# from projects.projects import app_projects # Blueprint directory import projects definition
 
-# setup App pages
-from projects.projects import app_projects # Blueprint directory import projects definition
+# # register URIs
+# app.register_blueprint(user_api) # register api routes
+# app.register_blueprint(app_projects) # register app pages
 
-
-# Initialize the SQLAlchemy object to work with the Flask app instance
-db.init_app(app)
-
-# register URIs
-app.register_blueprint(joke_api) # register api routes
-app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(user_api) # register api routes
-app.register_blueprint(player_api)
-app.register_blueprint(app_projects) # register app pages
-
-@app.route('/api/movies', methods=['GET'])
-def get_movies():
-    movies = os.listdir('/movies')
-    return jsonify(movies)
+# @app.route('/api/movies', methods=['GET'])
+# def get_movies():
+#     movies = os.listdir('/movies')
+#     return jsonify(movies)
 
 @app.errorhandler(404)  # catch for URL not found
 def page_not_found(e):
@@ -56,17 +41,11 @@ def table():
 def before_request():
     # Check if the request came from a specific origin
     allowed_origin = request.headers.get('Origin')
-    if allowed_origin in ['http://localhost:4100', 'http://127.0.0.1:4100', 'https://nighthawkcoders.github.io']:
+    if allowed_origin in ['http://localhost:8086', 'http://127.0.0.1:8086', 'https://nighthawkcoders.github.io']:
         cors._origins = allowed_origin
 
 # Create an AppGroup for custom commands
 custom_cli = AppGroup('custom', help='Custom commands')
-
-# Define a command to generate data
-@custom_cli.command('generate_data')
-def generate_data():
-    initUsers()
-    initPlayers()
 
 # Register the custom command group with the Flask application
 app.cli.add_command(custom_cli)
